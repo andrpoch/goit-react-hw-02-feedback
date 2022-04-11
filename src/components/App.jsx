f9b406ba72923b4b46d4da0ee197497cbb8a1b9e
andrpoch <andrpoch25@gmail.com>
2022-04-11 17:17:05 +0300

    Add section component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,23 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import FeedbackOptions from './FeedbackOptions';
+import Notification from './Notification';
+import Section from './Section';
+import Statistics from './Statistics';
+
+class App extends Component {
+  state = { 
+  good: 0,
+  neutral: 0,
+  bad: 0
+  }
+
+  
+  render() {
+    return (
+
+      <Notification message="There is no feedback"></Notification>
+    );
+  }
+}
+
+export default App;
